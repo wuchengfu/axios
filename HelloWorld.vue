106d39524ca11aa7269a4a0d5f7093d598e3df33
wcf123456 <2974290243@qq.com>
2019-04-26 10:51:55 +0800

    axios封装ip

---
+++ HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+	 import { getTest } from '../server/index.js'
+export default {
+ 
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+	mounted(){
+		getTest({
+		        url: 'topic/getTopic/105/138'
+		      })
+		      .then(response => {
+							console.log("成功")
+		       console.log(response.data)
+		 })
+
+		
+	}
+}
+</script>
+
+</style>
